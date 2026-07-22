98ffdb777d6eac2272f3dccd154ae149a9023584
ankitb798 <ankitbhardwaj6a@gmail.com>
2026-07-22 16:42:43 +0530

    .

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -3,9 +3,7 @@ import "./ChatInput.css";
 
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${GEMINI_API_KEY}`;
-console.log("API Key:", GEMINI_API_KEY);
-console.log("Length:", GEMINI_API_KEY?.length);
-console.log("URL:", GEMINI_URL);
+
 export function ChatInput({
   chatMessages,
   setChatMessages,
